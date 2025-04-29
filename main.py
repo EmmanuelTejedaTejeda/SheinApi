19317608bf3f937fc9f1c1b92323f471d6b410d9
EmmanuelTejedaTejeda <98726962+EmmanuelTejedaTejeda@users.noreply.github.com>
2025-04-29 08:25:42 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+import pymongo
+import json
+from fastapi.responses import JSONResponse
+from datetime import datetime
+from bson import ObjectId
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+client = pymongo.MongoClient("mongodb+srv://emmanuel2d28:omlydwtwu9RhJWTo@sheincluster.oddfa1j.mongodb.net/shein?retryWrites=true&w=majority")
+db = client["shein"]
+collection = db["shein_db"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins =["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+def serialize_document(doc):
+    for key, value in doc.items():
+        if isinstance(value, datetime):
+            doc[key]= value.isoformat()
+        elif isinstance(value, ObjectId):
+            doc[key] = str(value)
+    return doc
+
+@app.get("/getProductos")
+def get_productos():
+    documents =list(collection.find())
+    serialized_documents = [serialize_document(doc) for doc in documents]
+    return JSONResponse(content=serialized_documents)
