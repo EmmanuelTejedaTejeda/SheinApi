56bec4ca4accd924e0206342c649ad03f55e9346
EmmanuelTejedaTejeda <98726962+EmmanuelTejedaTejeda@users.noreply.github.com>
2025-05-14 12:34:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -47,6 +47,10 @@ def delete_clientes(id:str):
             raise HTTPException(status_code=404, detail="Cliente no encontrado")
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+@app.post("/clientes/createCliente")
+def create_clientes(request:Request):
+    
         
 
 @app.get("/getProductos")
